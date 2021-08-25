e5296751b531c646cbf3f117fcf2051f535b847b
muhammadfallahi <muhammadfallahi6@gmail.com>
2021-08-25 17:39:15 +0430

    do

---
+++ mirations/m0001_initial.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class m0001_initial {
+
+    public function up(){
+
+        echo "applying migration";
+    }
+
+    public function down(){
+
+        echo "down migration";
+    }
+}
+
